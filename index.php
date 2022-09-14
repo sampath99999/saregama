39c42f5e1c213eccd436b7571d10880532738fbc
Sampath Bandla <sampathbandla@lewisu.edu>
2022-09-14 23:05:27 +0530

    Index design

--- index.php
+++ index.php
@@ -4,9 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login || Saregama</title>
+    <title>Home || Saregama</title>
+
+    <link rel="stylesheet" href="./css/main.css">
 </head>
 <body>
-    
+    <div class="main">
+        <div class="sidebar">
+            <div class="header">
+                <div class="brand">Saregama</div>
+                <ul>
+                    <li class="song"></li>
+                </ul>
+            </div>
+        </div>
+        <div class="conten">
+
+        </div>
+    </div>
 </body>
 </html>
