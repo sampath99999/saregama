ee57cfb6b5d605e8897b77b2e98b7e20d4bb324f
Sampath Bandla <sampathbandla@lewisu.edu>
2022-09-15 12:55:21 +0530

    Changed colors and added new font

--- index.php
+++ index.php
@@ -14,7 +14,32 @@
             <div class="header">
                 <div class="brand">Saregama</div>
                 <ul>
-                    <li class="song"></li>
+                    <li class="active song">
+                        <div class="left">
+                            <img src="https://s1.dmcdn.net/v/QkVxK1TG_Emene1FI/x1080" alt="">
+                        </div>
+                        <div class="right">
+                            <p class="title">
+                                Lovely
+                            </p>
+                            <p class="singer">
+                                Billie Eilish
+                            </p>
+                        </div>
+                    </li>
+                    <li class="song">
+                        <div class="left">
+                            <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/episode-0862-pictured-singer-ariana-grande-performs-no-news-photo-953566612-1551210521.jpg?crop=0.766xw:0.575xh;0.0753xw,0.255xh&resize=1200:*" alt="">
+                        </div>
+                        <div class="right">
+                            <p class="title">
+                                Side to Side
+                            </p>
+                            <p class="singer">
+                                Ariana Grande
+                            </p>
+                        </div>
+                    </li>
                 </ul>
             </div>
         </div>
